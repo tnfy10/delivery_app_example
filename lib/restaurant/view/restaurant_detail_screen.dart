d50f5cd39327af4856b76727c83f81208ce82799
Hong Sunghwi <tnfy10@gmail.com>
2022-12-13 23:56:04 +0900

    sliver list 구현

--- lib/restaurant/view/restaurant_detail_screen.dart
+++ lib/restaurant/view/restaurant_detail_screen.dart
@@ -9,25 +9,48 @@ class RestaurantDetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return DefaultLayout(
-      title: '불타는 떡볶이',
-      child: Column(
-        children: [
-          RestaurantCard(
-            image: Image.asset('asset/img/food/ddeok_bok_gi.jpg'),
-            name: '불타는 떡볶이',
-            tags: ['떡볶이', '맛있음', '치즈'],
-            ratingsCount: 100,
-            deliveryTime: 30,
-            deliveryFee: 3000,
-            ratings: 4.76,
-            isDetail: true,
-            detail: '맛있는 떡볶이',
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16),
-            child: ProductCard(),
-          )
-        ],
+        title: '불타는 떡볶이',
+        child: CustomScrollView(
+          slivers: [_renderTop(), _renderLabel(), _renderProducts()],
+        ));
+  }
+
+  SliverPadding _renderLabel() {
+    return const SliverPadding(
+      padding: EdgeInsets.symmetric(horizontal: 16),
+      sliver: SliverToBoxAdapter(
+        child: Text('메뉴',
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500)),
+      ),
+    );
+  }
+
+  SliverPadding _renderProducts() {
+    return SliverPadding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      sliver: SliverList(
+          delegate:
+              SliverChildBuilderDelegate(childCount: 10, (context, index) {
+        return const Padding(
+          padding: EdgeInsets.only(top: 16),
+          child: ProductCard(),
+        );
+      })),
+    );
+  }
+
+  SliverToBoxAdapter _renderTop() {
+    return SliverToBoxAdapter(
+      child: RestaurantCard(
+        image: Image.asset('asset/img/food/ddeok_bok_gi.jpg'),
+        name: '불타는 떡볶이',
+        tags: ['떡볶이', '맛있음', '치즈'],
+        ratingsCount: 100,
+        deliveryTime: 30,
+        deliveryFee: 3000,
+        ratings: 4.76,
+        isDetail: true,
+        detail: '맛있는 떡볶이',
       ),
     );
   }
