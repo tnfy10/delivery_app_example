0031c40235874044b63e24f4028c160923e862fc
Hong Sunghwi <tnfy10@gmail.com>
2022-12-15 20:56:26 +0900

    Dio onError Interceptor 작업

--- lib/common/dio/dio.dart
+++ lib/common/dio/dio.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:delivery_app_example/common/const/data.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
@@ -29,5 +31,45 @@ class CustomInterceptor extends Interceptor {
     }
 
     return super.onRequest(options, handler);
+  }
+
+  @override
+  void onError(DioError err, ErrorInterceptorHandler handler) async {
+    print('[ERR] [${err.requestOptions.method}] ${err.requestOptions.uri}');
+
+    final refreshToken = await storage.read(key: refreshTokenKey);
+
+    if (refreshToken == null) {
+      return handler.reject(err);
+    }
+
+    final isStatus401 = err.response?.statusCode == HttpStatus.unauthorized;
+    final isPathRefresh = err.requestOptions.path == '/auth/token';
+
+    if (isStatus401 && isPathRefresh) {
+      final dio = Dio();
+
+      try {
+        final resp = await dio.post('http://$ip/auth/token',
+            options:
+                Options(headers: {'authorization': 'Bearer $refreshToken'}));
+
+        final accessToken = resp.data['accessToken'];
+
+        final options = err.requestOptions;
+
+        options.headers.addAll({'authorization': 'Bearer $accessToken'});
+
+        await storage.write(key: accessTokenKey, value: accessToken);
+
+        final response = await dio.fetch(options);
+
+        return handler.resolve(response);
+      } on DioError catch (e) {
+        return handler.reject(e);
+      }
+    }
+
+    return handler.reject(err);
   }
 }
