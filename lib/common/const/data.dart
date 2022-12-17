ffaa68926a65715f7b6e14206da78bf7c6412d1c
Hong Sunghwi <tnfy10@gmail.com>
2022-12-18 01:21:30 +0900

    외부 접속 가능한 url로 변경

--- lib/common/const/data.dart
+++ lib/common/const/data.dart
@@ -1,10 +1,4 @@
-import 'dart:io';
-
 const accessTokenKey = 'ACCESS_TOKEN';
 const refreshTokenKey = 'REFRESH_TOKEN';
 
-// TODO: 별도 서버 돌려서 외부 접속 가능하게 변경
-const emulatorIp = '10.0.2.2:3000';
-const simulatorIp = '127.0.0.1:3000';
-
-final ip = Platform.isIOS ? simulatorIp : emulatorIp;
+const ip = 'delivery.myeoru.xyz';
