e3ef08bb75b393ecce7bf701e36048d7e5476c8b
Hong Sunghwi <tnfy10@gmail.com>
2022-12-22 22:28:43 +0900

    RestaurantRatingRepository 작업

---
+++ lib/restaurant/repository/restaurant_rating_repository.dart
@@ -0,0 +1,19 @@
+import 'package:dio/dio.dart' hide Headers;
+import 'package:retrofit/http.dart';
+
+import '../../common/model/cursor_pagination_model.dart';
+import '../../common/model/pagination_params.dart';
+import '../../rating/model/rating_model.dart';
+
+part 'restaurant_rating_repository.g.dart';
+
+@RestApi()
+abstract class RestaurantRatingRepository {
+  factory RestaurantRatingRepository(Dio dio, {String baseUrl}) = _RestaurantRatingRepository;
+
+  @GET('/')
+  @Headers({'accessToken': 'true'})
+  Future<CursorPagination<RatingModel>> paginate({
+    @Queries() PaginationParams? paginationParams = const PaginationParams(),
+  });
+}
