fea95e5bcbd6237aa05edb8e68485d5a96b8b2b9
Hong Sunghwi <tnfy10@gmail.com>
2023-01-08 22:26:30 +0900

    OrderRepository 작업

---
+++ lib/order/repository/order_repository.dart
@@ -0,0 +1,30 @@
+import 'package:delivery_app_example/common/dio/dio.dart';
+import 'package:delivery_app_example/order/model/post_order_body.dart';
+import 'package:dio/dio.dart' hide Headers;
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:retrofit/http.dart';
+
+import '../../common/const/data.dart';
+import '../model/order_model.dart';
+
+part 'order_repository.g.dart';
+
+final orderRepositoryProvider = Provider<OrderRepository>((ref) {
+  final dio = ref.watch(dioProvider);
+
+  return OrderRepository(dio, baseUrl: 'http://$ip/order');
+});
+
+// http://$ip/order
+@RestApi()
+abstract class OrderRepository {
+  factory OrderRepository(Dio dio, {String baseUrl}) = _OrderRepository;
+
+  @POST('/')
+  @Headers({
+    'accessToken': 'true',
+  })
+  Future<OrderModel> postOrder({
+    @Body() required PostOrderBody body,
+  });
+}
